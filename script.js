42c8162360990a119f4d3611e0c92c4db07494e3
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-08 15:24:19 +0100

    update

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 let snake = document.querySelector('#snake');
 let btns = document.querySelectorAll('div#btns p');
 let position = 0;
-let clickedBtn = null;
+
+let clickedBtn = '';
 
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
         clickedBtn = btn.id;
 
-        setInterval(() => {
-            switch (clickedBtn) {
-                case 'right':
-                    clickedBtn !== '' ? move('marginLeft', 10) : clickedBtn = '';
-                
-                  break;
-                case 'left':
-                   move('marginLeft', -10);
-                   
-                  break;
-            }
-            
-        }, 500);
     });
 });
+
+
+setInterval(() => {
+    switch (clickedBtn) {
+        case 'right':
+           move('marginLeft', 10);
+          break;
+        case 'left':
+           move('marginLeft', -10);
+          break;
+        case 'down':
+           move('marginTop', 10);
+          break;
+        case 'up':
+           move('marginTop', -10);
+          break;
+    }
+    
+}, 500);
+
 
 function move(index, speed) {
     if ( position > 260 || position < 0 ){
